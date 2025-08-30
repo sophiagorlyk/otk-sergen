cf76ba5cca477131d73473ffe0d45a814c6a7845
gorlyk <sofiya.melisa07@gmail.com>
2025-08-31 02:56:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.getElementById("button1").addEventListener("click", () => {
     window.open("button1.html", "_blank");
 });
  
-document.getElementById("button2").addEventListener("click", () => {
-    window.open("button2.html", "_blank");
-});
+
 
 
